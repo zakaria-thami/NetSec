4b0fbdada9c270345f961473eac3aa24088b319a
MatteoTuzi <Matteo.Tuzi00@gmail.com>
2025-01-22 23:52:38 +0100

    added env file and gitignore

--- ns-analyzer-be/main.py
+++ ns-analyzer-be/main.py
@@ -1,6 +1,11 @@
 # backend/app.py
+import os
 from flask import Flask, jsonify
 from flask_cors import CORS
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS for frontend communication
@@ -10,4 +15,5 @@ def home():
     return jsonify({"message": "Hello from Flask API!"})
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.getenv("FLASK_PORT", 4000))  # Default to 5000 if not set
+    app.run(debug=True, host='0.0.0.0', port=port)
