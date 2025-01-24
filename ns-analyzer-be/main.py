cc2b33ddb061f1875c753a9fc34ef14a1ef46668
zakaria <thami.zakaria.dev@gmail.com>
2025-01-24 08:52:04 -0500

    Home page + templates and static directory

--- ns-analyzer-be/main.py
+++ ns-analyzer-be/main.py
@@ -1,18 +1,22 @@
 # backend/app.py
 import os
-from flask import Flask, jsonify
+from flask import Flask,render_template, jsonify
 from flask_cors import CORS
 from dotenv import load_dotenv
-
+	
 # Load environment variables from .env file
 load_dotenv()
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS for frontend communication
 
+
+
+
 @app.route('/')
 def home():
-    return jsonify({"message": "Hello from Flask API!"})
+    #return render_template(template('Home.html'))
+    return render_template("Home.html")
 
 if __name__ == '__main__':
     port = int(os.getenv("FLASK_PORT", 4000))  # Default to 5000 if not set
